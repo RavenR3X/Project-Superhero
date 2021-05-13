b34036d847051d6deea1325890365f3c4b9225e2
RavenR3X <78075608+RavenR3X@users.noreply.github.com>
2021-05-13 11:36:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,34 @@
-var canvas = new fabric.Canvas('myCanvas')
+var canvas = new fabric.Canvas('myCanvas');
 
+canvas_width = 30;
+canvas_height = 30;
+
+player_x=10;
+player_y=10;
+
+var player_object= "";
+var block_image_object= "";
+
+function player_update()
+{
+Image.fromURL("player.png", function(Img) {
+player_object = Img;
+
+player_object.scaleToWidth(150);
+player_object.scaleToHeight(140);
+player_object.set({
+top:player_y,
+left:player_x
+});
+canvas.add(player_object);
+
+});
+}
+
+block_image_object.scaleToWidth(block_image_width);
+
+block_image_object.scaleToHeight(block_image_height);
+block_image_object.set({
+    top:player_y,
+    left:player_x
+});
